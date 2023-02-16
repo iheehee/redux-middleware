280706fca9120930dfaa6f8c2217f25f1bf1a813
iheehee <iheeheedev@gmail.com>
2023-02-16 13:17:28 +0900

    axios 연습

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware'
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger()
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/lib/loggerMiddleware.js
+++ src/lib/loggerMiddleware.js
@@ -1,10 +1,10 @@
-const loggerMiddleware = store => next => action => {
-    console.group(action.type);
-    console.log('이전 상태', store.getState());
-    console.log('액션', action);
-    next(action); // 다음 미들웨어 혹은 리듀서에게 전달
-    console.log('다음 상태', store.getState());
-    console.groupEnd(); // 그룹 끝    
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log("이전 상태", store.getState());
+  console.log("액션", action);
+  next(action); // 다음 미들웨어 혹은 리듀서에게 전달
+  console.log("다음 상태", store.getState());
+  console.groupEnd(); // 그룹 끝
 };
 
 export default loggerMiddleware;
